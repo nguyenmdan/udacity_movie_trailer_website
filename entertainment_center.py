e57d92d3cdb939db5ccd0351d7ee4ceea579fa28
Daniel Nguyen <somerandomguy61@gmail.com>
2017-10-30 17:30:09 -0700

    Fixed PEP8 formatting errors

--- entertainment_center.py
+++ entertainment_center.py
@@ -3,22 +3,35 @@ import fresh_tomatoes
 
 # Movie() args are in format title, storyline, poster url, and youtube url
 
-annie_hall = media.Movie("Annie Hall",
-							" Neurotic New York comedian Alvy Singer falls in love with the ditzy Annie Hall.",
-							"https://sc-events.s3.amazonaws.com/4181858/main.jpg",
-							"https://www.youtube.com/watch?v=OqVgCfZX-yE")
+annie_hall = media.Movie(
+    "Annie Hall",
+    """Neurotic New York comedian Alvy Singer falls in love
+    with the ditzy Annie Hall.""",
+    "https://sc-events.s3.amazonaws.com/4181858/main.jpg",
+	"https://www.youtube.com/watch?v=OqVgCfZX-yE")
 
-kill_bill = media.Movie("Kill Bill Vol 1 & Vol 2",
-						"The Bride wakens from a four-year coma. The child she carried in her womb is gone. Now she must wreak vengeance on the team of assassins who betrayed her - a team she was once part of.",
-						"https://i.pinimg.com/736x/64/82/ef/6482ef51ce0b437986ec477e0d46f30c--quentin-tarantino-tarantino-movies.jpg",
-						"https://www.youtube.com/watch?v=ot6C1ZKyiME")
+kill_bill = media.Movie(
+	"Kill Bill Vol 1 & Vol 2",
+	"""The Bride wakens from a four-year coma.
+	The child she carried in her womb is gone.
+	Now she must wreak vengeance on the team of assassins who betrayed her -
+	a team she was once part of.""",
+	"""https://i.pinimg.com/736x/64/82/ef/6482ef51ce0b437986ec477e0d46f30c--quentin-tarantino-tarantino-movies.jpg""",  # noqa
+	"https://www.youtube.com/watch?v=ot6C1ZKyiME")
 
-arsenic_and_old_lace = media.Movie("Arsenic and Old Lace",
-									"A drama critic learns on his wedding day that his beloved maiden aunts are homicidal maniacs, and that insanity runs in his family.",
-									"http://avalonlibrary.org/wp-content/uploads/2014/04/arsenic-and-old-lace.jpg",
-									"https://www.youtube.com/watch?v=GCWBDwkhGN0")
+arsenic_and_old_lace = media.Movie(
+	"Arsenic and Old Lace",
+	"""A drama critic learns on his wedding day that his
+	beloved maiden aunts are homicidal maniacs,
+	and that insanity runs in his family.""",
+	"http://avalonlibrary.org/wp-content/uploads/2014/04/arsenic-and-old-lace.jpg",  # noqa
+	"https://www.youtube.com/watch?v=GCWBDwkhGN0")
 
 # All movies above are compiled into a list and passed onto open_movies_page()
-movies_list = [annie_hall, kill_bill, arsenic_and_old_lace]
+movies_list = [
+	annie_hall,
+	kill_bill,
+	arsenic_and_old_lace
+]
 
 fresh_tomatoes.open_movies_page(movies_list)
--- media.py
+++ media.py
@@ -1,6 +1,18 @@
-#Note: fresh_tomatoes uses var names title, poster_image_url and trailer_youtube_url
+"""
+Movie object containing various info on the movie
+
+Arguments:
+title_input: Title of the movie
+storyline_input: Brief summary of the movie
+poster_image_input: Link to movie poster. Must be valid image extension
+youtube_trailer_input: Link to trailer. Must be a youtube link
+"""
+# fresh_tomatoes uses var names title, poster_image_url and trailer_youtube_url
+
+
 class Movie:
-	def __init__(self, title_input, storyline_input, poster_image_input, youtube_trailer_input):
+	def __init__(self, title_input, storyline_input,
+				poster_image_input, youtube_trailer_input):
 		self.title = title_input
 		self.storyline = storyline_input
 		self.poster_image_url = poster_image_input
